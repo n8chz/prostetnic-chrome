89b5f0bce72cba7dc3782a8518bcaac71a7eaeb1
Lorraine Lee <n8chz@yahoo.ca>
2015-10-13 09:18:04 -0400

    removed a console.log()

--- rehilite.js
+++ rehilite.js
@@ -5,7 +5,6 @@ var partialURL = document.location.href.split(":")[1].split("#")[0];
 
 chrome.storage.local.get(partialURL, function (items) {
   if (items[partialURL]) {
-   console.log("items[partialURL]: "+JSON.stringify(items[partialURL]));
    items[partialURL].hilites.forEach(function (hiliteRecord) {
      window.find(hiliteRecord.text, false, false, false, false, false, false);
      hiliteSelection();
