1eba2b90d98016cb9b413f2c6556dada5f89700e
Lorraine Lee <n8chz@yahoo.ca>
2016-09-21 22:33:36 -0400

    made it so color picker window closes properly

--- picker.js
+++ picker.js
@@ -13,17 +13,28 @@ $(function () {
     update("color", $("#done").css("color"));
   });
 
+  // Each of the buttons with A's in them has class "change".
+  // They also have a class of either "color" or "background-color".
+  // Each button will change either the foreground or background color to
+  // match its own value for whichever of those two properties.
   $(".change").click(function () {
     var property = $(this).hasClass("color") ? "color" : "background-color";
     value = $(this).data("color");
     update(property, value);
   });
 
+  // The button with "Done" on it stores the selected style in storage as the
+  // value associated with the key "$style"
   $("#done").click(function () {
     chrome.storage.local.set({
       "$style": $(this).attr("style")
     }, function () {
-      chrome.windows.remove(chrome.windows.WINDOW_ID_CURRENT);
+      // kludge because in versions < 50, chrome.windows.remove() won't accept
+      // a negative value, including the constant
+      // chrome.windows.WINDOW_ID_CURRENT, which has a value of -2.
+      chrome.windows.get(chrome.windows.WINDOW_ID_CURRENT, function (w) {
+        chrome.windows.remove(w.id);
+      });
     });
   });
 
--- popup.js
+++ popup.js
@@ -23,16 +23,22 @@ keywordHandler = function (items) {
 
 var prev = "";
 
+// Every time contents of text input change, add button if the input contains a
+// word which is a storage key.  The button created will expand into a list of
+// URLs in which that word has been highlighted.
 searchInput.addEventListener("input", function (event) {
   event.preventDefault();
   var input = searchInput.value;
+  // Reset input if user has typed a whitespace character
   if (input.match(/\s$/)) searchInput.value = "";
   else {
    var word = input.toLowerCase();
    word = word.replace(/[\x00-\x26\x28-\x2f\x3a-\x60\x7b-\x7f]/g, "");
    searchInput.value = word;
    if (input.length && input != prev) chrome.storage.local.get(word, function (items) {
-     if (word.length && items[word] instanceof Array && items[word].length) { // word.length test should not be necessary! TODO: Check save.js & make sure we aren't using "" as a storage key!
+     if (word.length && items[word] instanceof Array && items[word].length) {
+      // word.length test should not be necessary!
+      // TODO: Check save.js & make sure we aren't using "" as a storage key!
       var keyword = document.createElement("fieldset");    
       var button = document.createElement("button");
       button.textContent = word+" ("+items[word].length+")";
